fe5033f05c1892ee55f481a005540c33b77f41f3
Andreas Geyer <andreas@qbitone.de>
2021-07-09 11:57:06 +0200

    includes docstring for button template function

--- template-parts/button.php
+++ template-parts/button.php
@@ -6,6 +6,27 @@
 
 
 if (!function_exists('quantum_button')) :
+    /**
+     * Button Template
+     *
+     * Output a html button with given parameters
+     *
+     * Example
+     *      $button_args = [
+     *          'href' => 'https://google.com',
+     *          'class' => 'button-class',
+     *          'button_text' => MyButton,
+     *      ];
+     *      quantum_botton($button_args);
+     *
+     *
+     * @param array $args - Paramters for the specific button
+     * @return void
+     *
+     * @param string href - the URL of the linking <a> tag
+     * @param string class - a class name to specify the button style
+     * @param string button_text - the Name of the clickabel button
+     */
     function quantum_button(array $args = array()): void
     {
 
