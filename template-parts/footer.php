2c6d7cc83cbd174f6f64b5af1b7e44d368b1b367
Andreas Geyer <andreas@qbitone.de>
2021-08-01 13:50:19 +0200

    adds template part for the footer

---
+++ template-parts/footer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Template part for the footer
+ */
+?>
+
+<footer id="footer" class="">
+
+    <div class="footerbar">
+
+        <div>
+            <?php quantum_footerbar_copyright(); ?>
+        </div>
+
+        <div>
+            <?php quantum_footerbar_meta(); ?>
+        </div>
+
+        <div>
+            <?php quantum_footerbar_menu(); ?>
+        </div>
+
+    </div><!-- .footerbar -->
+
+</footer><!-- #footer -->
