8decbf3704709be17860d333ef6f45e6e9a22c2a
Andreas Geyer <andreas@qbitone.de>
2021-11-11 20:22:42 +0100

    minor changes

--- inc/utility-functions.php
+++ inc/utility-functions.php
@@ -84,7 +84,7 @@ if (!function_exists('get_quantum_button')) :
      * Get a button template.
      * 
      * Shortcut function to get a button via get_template_part
-     * function
+     * function.
      *
      * @param string $name name of the specific button
      * @param array $args further details/attributes of the button
