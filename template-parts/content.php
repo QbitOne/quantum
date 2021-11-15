f8937d926a8969e4c19cd3785b26c0b35acff3a3
Andreas Geyer <andreas@qbitone.de>
2021-11-15 14:48:12 +0100

    restructures code

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,47 +8,78 @@
  * @package Quantum
  */
 
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class((!is_singular() ? 'qu-flex-grid__item' : '')); ?>>
+
 	<header class="entry-header">
-		<?php
-		if (is_singular()) :
-			the_title('<h1 class="entry-title">', '</h1>');
-		else :
-			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-		endif;
-		?>
+
+		<?php if (is_singular()) : ?>
+
+			<h1 class="entry-title"> <?php the_title() ?> </h1>
+
+		<?php else : ?>
+
+			<h2 class="entry-title">
+
+				<a href="<?php echo esc_url(get_permalink()) ?>" rel="bookmark">
+
+					<?php the_title(); ?>
+
+				</a>
+
+			</h2>
+
+		<?php endif ?>
+
 	</header><!-- .entry-header -->
+
 
 	<?php quantum_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__('Weiterlesen <span class="screen-reader-text">von "%s"</span>', 'quantum'),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post(get_the_title())
-			)
-		);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__('Pages:', 'quantum'),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+	<?php if (is_singular()) : ?>
+
+		<div class="entry-content">
+
+			<?php the_content() ?>
+
+			<?php
+			wp_link_pages(
+				[
+					'before' => '<div class="page-links">' . esc_html__('Pages:', 'quantum'),
+					'after'  => '</div>',
+				]
+			);
+			?>
+
+		</div><!-- .entry-content -->
+
+	<?php endif ?>
+
+
+	<?php if (!is_singular() && has_excerpt()) :  ?>
+
+		<div class="entry-excerpt">
+
+			<?php the_excerpt(); ?>
+
+		</div><!-- .entry-excerpt -->
+
+	<?php endif ?>
+
+
+	<?php if (is_singular()) : ?>
+		<hr>
+	<?php endif; ?>
+
 
 	<footer class="entry-footer">
+
 		<?php quantum_entry_footer(); ?>
+
 	</footer><!-- .entry-footer -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
