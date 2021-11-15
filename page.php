1544bfdfead01d7305facaba60fa75f61e3ab347
Andreas Geyer <andreas@qbitone.de>
2021-11-15 13:56:36 +0100

    adds container to template files

--- page.php
+++ page.php
@@ -16,22 +16,26 @@
 get_header();
 ?>
 
+<div class="qu-container">
+
+    <?php
+    while (have_posts()) :
+        the_post();
+
+        get_template_part('template-parts/content', 'page');
+
+        // If comments are open or we have at least one comment, load up the comment template.
+        if (comments_open() || get_comments_number()) :
+            comments_template();
+        endif;
+
+    endwhile; // End of the loop.
+    ?>
+
+    <?php get_sidebar(); ?>
+
+</div>
 
 <?php
-while (have_posts()) :
-    the_post();
 
-    get_template_part('template-parts/content', 'page');
-
-    // If comments are open or we have at least one comment, load up the comment template.
-    if (comments_open() || get_comments_number()) :
-        comments_template();
-    endif;
-
-endwhile; // End of the loop.
-?>
-
-
-<?php
-get_sidebar();
 get_footer();
