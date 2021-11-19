5c924a63831612bb0fc7a77c82b94b3c81bf3dfd
Andreas Geyer <andreas@qbitone.de>
2021-11-19 15:21:25 +0100

    adds flex classes

--- archive.php
+++ archive.php
@@ -21,16 +21,18 @@ get_header();
         ?>
     </header><!-- .page-header -->
 
-<?php
+    <div class="qu-flex qu-flex-gri cols-3">
+
+    <?php
     /* Start the Loop */
     while (have_posts()) :
         the_post();
 
         /*
-                * Include the Post-Type-specific template for the content.
-                * If you want to override this in a child theme, then include a file
-                * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-                */
+        * Include the Post-Type-specific template for the content.
+        * If you want to override this in a child theme, then include a file
+        * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+        */
         get_template_part('template-parts/content', get_post_type());
 
     endwhile;
@@ -42,10 +44,11 @@ else :
     get_template_part('template-parts/content', 'none');
 
 endif;
-?>
+    ?>
+
+    </div>
 
 
-
-<?php
-get_sidebar();
-get_footer();
+    <?php
+    get_sidebar();
+    get_footer();
