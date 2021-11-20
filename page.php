97ef4b9a8b2a256d328e65494f7a09f4432efb67
Andreas Geyer <andreas@qbitone.de>
2021-11-20 08:25:32 +0100

    deletes comment func for pages

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The template for displaying all pages
+ * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -24,11 +24,6 @@ get_header();
 
         get_template_part('template-parts/content', 'page');
 
-        // If comments are open or we have at least one comment, load up the comment template.
-        if (comments_open() || get_comments_number()) :
-            comments_template();
-        endif;
-
     endwhile; // End of the loop.
     ?>
 
@@ -37,5 +32,4 @@ get_header();
 </div>
 
 <?php
-
 get_footer();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying page content in page.php.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -10,45 +11,30 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </header><!-- .entry-header -->
 
-    <?php quantum_post_thumbnail(); ?>
+	<?php if (get_edit_post_link()) : ?>
+		<?php quantum_edit_post_link() ?>
+	<?php endif; ?>
 
-    <div class="entry-content">
-        <?php
-		the_content();
+	<header class="entry-header">
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'quantum' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-    </div><!-- .entry-content -->
+		<h1 class="entry-title">
+			<?php the_title(); ?>
+		</h1>
 
-    <?php if ( get_edit_post_link() ) : ?>
-    <footer class="entry-footer">
-        <?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'quantum' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-    </footer><!-- .entry-footer -->
-    <?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php quantum_post_thumbnail(); ?>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php if (get_edit_post_link()) : ?>
+			<?php quantum_edit_post_link() ?>
+		<?php endif; ?>
+	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
